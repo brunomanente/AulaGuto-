4a89f24066c74b83b9462514a30b9f2d0799e063
brunomanente <bruno.manente0@gmail.com>
2019-02-13 21:44:14 -0200

    Aula de hoje 13 de fevereiro C#

---
+++ Estudo dirigido/Variaveis/Variaveis/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int x;
+            int y;
+
+            x = 7;
+            y = x + 3;
+
+            Console.WriteLine(y);
+            Console.ReadLine();
+            */
+
+            Console.WriteLine("qual é o seu nome");
+            Console.WriteLine("qual é o seu sobrenome");
+            string myFirstName;
+            myFirstName = Console.ReadLine();
+
+            string myLastName;
+            Console.Write("Me informe o seu sobrenome?");
+            myLastName = Console.ReadLine();
+
+            Console.WriteLine("olá," + myFirstName + " " + myLastName);
+            Console.ReadLine();
+        }
+    }
+}
